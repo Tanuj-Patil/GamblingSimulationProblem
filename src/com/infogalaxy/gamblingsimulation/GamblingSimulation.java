f0c07995a4574c9cb3b32d5dbd01dcfe364f599c
TanujPatil <tanujpatil9561@gmail.com>
2024-03-06 10:00:03 +0530

    [ADDED]:by Tanuj - Setting up Stake and Bet

---
+++ src/com/infogalaxy/gamblingsimulation/GamblingSimulation.java
@@ -0,0 +1,13 @@
+package com.infogalaxy.gamblingsimulation;
+
+public class GamblingSimulation {
+
+	private static final int STAKE_PER_DAY = 100;
+	private static final int BET_PER_GAME = 1;
+	
+	public static void main(String[] args) {
+		System.out.println("The Stake per day is: " +STAKE_PER_DAY);
+		System.out.println("The Bet per game is: " +BET_PER_GAME);
+	}
+
+}
