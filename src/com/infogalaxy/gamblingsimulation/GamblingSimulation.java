c74f00c502a31846efb825ae778baea1c385fbdb
TanujPatil <tanujpatil9561@gmail.com>
2024-03-06 22:22:35 +0530

    [ADDEd]:by Tanuj - Calculated Luckiest and Unluckiest day

--- src/com/infogalaxy/gamblingsimulation/GamblingSimulation.java
+++ src/com/infogalaxy/gamblingsimulation/GamblingSimulation.java
@@ -18,41 +18,69 @@ public class GamblingSimulation {
 		Random random = new Random();
 		int[] winAmount = new int[20];
 		int[] looseAmount = new int[20];	
-	
-		int wonDays = 0;
+		int[] maxLost = new int[20];
+		int[] maxWon = new int[20];
+		
 		int lostDays = 0;
+		int wonDays = 0;
+		int maxL = 0;
+		int maxW = 0;
+		int indexL = 0;
+		int indexW = 0;
+		
 		for(int i = 0; i < 20; i++) {
+			int winStake = 0;
+			int looseStake = 0;
+
 			System.out.println("Day: " +(i+1));
-			int winStack = 0;
-			int looseStack = 0;
+			
 			while(true) {
 //				gameStatus();
 				int playStatus = random.nextInt(9) % 2;
 //				System.out.println("Play Status: " +playStatus);
 				if(playStatus == LOOSE) {
-					looseStack += 1;
+					looseStake += 1;
 				} else {
-					winStack += 1;			
+					winStake += 1;			
 				}
 				
-				if(looseStack == STAKE_PER_DAY / 2) {
+				if(looseStake == STAKE_PER_DAY / 2) {
 					lostDays++;
+					if(looseStake > winStake) {
+						maxLost[i] = looseStake - winStake;
+						if(maxL < maxLost[i]) {
+							maxL = maxLost[i];
+							indexL = i+1;
+						}
+					}
 					break;
 				}
-				if(winStack == STAKE_PER_DAY / 2) {
+				if(winStake == STAKE_PER_DAY / 2) {
 					wonDays++;
+					if(looseStake < winStake) {
+						maxWon[i] = winStake - looseStake;
+						if(maxW < maxWon[i]) {
+							maxW = maxWon[i];
+							indexW = i+1;
+						}
+					}
 					break;
-				}
-				
+				}	
 			}
-			looseAmount[i] = looseStack;
-			winAmount[i] = winStack;
+			looseAmount[i] = looseStake;
+			winAmount[i] = winStake;
 //			System.out.println("Winnig Stack: " +winStack);
-//			System.out.println("Loosing Stack: " +looseStack);	
+//			System.out.println("Loosing Stack: " +looseStack);
+			
+//			System.out.println("MaxLost: " +maxLost[i]);
+//			System.out.println("MaxWon: " +maxWon[i]);
+			
 			System.out.println("Loose Amount: " +looseAmount[i]);
 			System.out.println("Win Amount: " +winAmount[i]);
 			System.out.println("------------------------");	
 		}
+		System.out.println(indexL+ " is the Unluckiest day, lost rs: " +maxL);
+		System.out.println(indexW+ " is the Luckiest day, won rs: " +maxW);
 		System.out.println("Total Days Lost: " +lostDays);
 		System.out.println("Total Days Won: " +wonDays);
 	}
